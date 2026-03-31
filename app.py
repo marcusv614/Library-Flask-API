38b91bcde423353f257dfcf629b5013021c187c6
Desktop Marcus <marcus.pinheiro@super.ufam.edu.br>
2026-03-31 12:29:06 -0400

    feat(app): adicionar endpoint POST /books para criação de livro

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from db import db
 from flask_migrate import Migrate 
+from models.book import Book
 
 app = Flask(__name__)
 
@@ -9,6 +10,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app,db)
+
+@app.route("/books",methods=['POST'])
+def add_book():
+    data = request.get_json()
+    newbook = Book(title=data['title'],author=data['author'],description=data['description']) # type: ignore
+    db.session.add(newbook)
+    db.session.commit()
+    return jsonify({'message: livro inserido com sucesso' }), 201
 
 if __name__ == "__main__":
     app.run(debug=True,port=8080,host="0.0.0.0")
