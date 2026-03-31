3144a1761b8a9def49d9625b1fa93099e0238184
Desktop Marcus <marcus.pinheiro@super.ufam.edu.br>
2026-03-31 12:08:47 -0400

    feature: adicionar migrations

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from db import db
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/book'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app,db)
 
 if __name__ == "__main__":
     app.run(debug=True,port=8080,host="0.0.0.0")
