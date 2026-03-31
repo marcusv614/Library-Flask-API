6ac52c28cd33e4971c2ea863d913d7ff2a1d5538
Desktop Marcus <marcus.pinheiro@super.ufam.edu.br>
2026-03-31 11:52:05 -0400

    feature: configurar SQLALCHEMY Define URI para acesso do DB Configura SQLALCHEMY para utilizar migrations

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/book'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 if __name__ == "__main__":
     app.run(debug=True,port=8080,host="0.0.0.0")
